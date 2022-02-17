abd83455e69336bd5b0e1915a9b4dcc3b776a699
abhinavsiwal <abhinavsiwal@gmail.com>
2022-02-17 14:37:17 +0530

    Backend: Seller admin routes complete

--- Backend/routes/seller.js
+++ Backend/routes/seller.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 const router = express.Router();
 
-const {signup,login,verifyOtp} = require('../controllers/seller-controller')
+const {signup,login,verifyOtp, allSellers, getSellerDetails, deleteSeller} = require('../controllers/seller-controller')
 const {isAuthenticatedSeller} = require("../middlewares/checkSellerAuth");
 const {isAdmin} = require("../middlewares/checkAdmin");
 
@@ -9,6 +9,9 @@ const {isAdmin} = require("../middlewares/checkAdmin");
 router.post("/signup",signup);
 router.post("/login",login);
 router.post("/verifyOtp",verifyOtp);
+router.get("/sellers",isAuthenticatedSeller,isAdmin,allSellers);
+router.get("/sellers/:id",isAuthenticatedSeller,isAdmin,getSellerDetails);
+router.delete("/seller/:id",isAuthenticatedSeller,isAdmin,deleteSeller);
 
 
 module.exports=router;
